b20a0412caebfc0b92fb321e5c4201ddc4524ae3
Przemek8686 <1986krawiec1986@gmail.com>
2023-09-16 18:59:02 +0200

    correct Title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
  export const Title = () => {
   return (
     <div className="title">
-      <h1>Galeria Zdjeć</h1>
+      <h1>Galeria Zdjec</h1>
       
       </div>
   );
